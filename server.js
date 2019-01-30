f8fc743aaa721cde6bb02421bbf2a7d6d9ac23b7
Idrak Mustahsin <playbox8g@gmail.com>
2019-01-30 20:37:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -73,14 +73,15 @@ app.post('/videos', authenticate, (req, res) => {
 	newVideo.save().then((doc) => {
 		res.send(doc);
 	}).catch(e => {
-		res.send(e);
+		res.send(e);]
 	});
     }else {
 	 collection.find({name:newVideo.collectionName}).then(col => {
-		col.videos.push(newVideo).then(doc => {
-			res.send(doc)
+		col.videos.push(newVideo);
+		col.save().then(doc => {
+			res.send(doc);	
 		}).catch(e => {
-			res.send(e)
+			res.send(e);	
 		});
 	 });
     }  
